222ccd6fb6cbcd265beec413acb124566abdd90c
kimo741 <kimoelgamal741@gmail.com>
2022-11-30 18:48:07 +0200

    complet spacific product component

--- src/components/shops/vendor-page/SpacificProduct.vue
+++ src/components/shops/vendor-page/SpacificProduct.vue
@@ -65,7 +65,7 @@
     <!-- ///////// -->
     <!-- more adds -->
     <!-- ///////// -->
-    <q-card class="q-my-md q-pa-md main-rounded">
+    <q-card class="q-my-md q-pa-md main-rounded card-shadow">
       <div class="text-right text-body1 q-pb-sm text-primary text-500">
         اختيارك من الاصناف الجانبية :
       </div>
@@ -90,7 +90,7 @@
         </q-item>
       </q-list>
     </q-card>
-    <q-card class="q-my-md q-pa-md main-rounded">
+    <q-card class="q-my-md q-pa-md main-rounded card-shadow">
       <div class="text-right text-body1 q-pb-sm text-primary text-500">
         الاضافات <span class="text-hint text-sm">(اختياري) :</span>
       </div>
@@ -116,6 +116,40 @@
         </q-item>
       </q-list>
     </q-card>
+    <div class="button__section row items-center">
+      <div @click="addToCart" class="col-5 flex items-center add-tocart">
+        <!-- <Transition class="col-2" name="slide-up"> -->
+        <q-icon
+          v-if="my_cart"
+          color="primary"
+          class="col-2"
+          size="md"
+          name="done_all"
+        />
+        <q-icon v-else color="primary" class="col-2" size="md" name="add" />
+        <!-- </Transition> -->
+        <div class="col-10 text-sm">أضف الي السلة</div>
+      </div>
+      <div class="col-2 text-sm text-center row items-center">
+        <div class="text-body2 text-500">400 ر.س</div>
+      </div>
+      <div class="col-4">
+        <div class="row text-no-wrap order-count items-center">
+          <!-- ///////////////// -->
+          <!-- button add 1 more -->
+          <!-- ///////////////// -->
+          <div @click="increaseOrder" class="order-count--sum"></div>
+          <!-- //////////// -->
+          <!-- order counte -->
+          <!-- //////////// -->
+          <div class="col text-center text-bold">{{ count }}</div>
+          <!-- /////////////////// -->
+          <!-- buttun minus 1 more -->
+          <!-- /////////////////// -->
+          <div @click="decreaseOrder" class="order-count--minus"></div>
+        </div>
+      </div>
+    </div>
   </q-card>
 </template>
 
@@ -125,7 +159,9 @@ import { ref } from "vue";
 export default {
   setup() {
     return {
+      my_cart: ref(false),
       side_items: ref("1"),
+      count: ref("1"),
       add_more: ref([]),
       like_it: ref(false),
       model: ref("4"),
@@ -169,14 +205,26 @@ export default {
     // markItem(item) {
     //   this.add_more.push(item.val);
     // },
+    increaseOrder() {
+      this.count++;
+    },
+    decreaseOrder() {
+      if (this.count > 1) {
+        this.count--;
+      }
+    },
+    addToCart() {
+      this.my_cart = !this.my_cart;
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
 .single-product {
-  background: rgba(255, 255, 255, 0.95);
-  padding: 20px;
+  background-color: #f5f4f3;
+  // background: rgba(255, 255, 255, 0.95);
+  padding: 10px;
   .close-icon {
     position: absolute;
     right: 8px;
@@ -200,5 +248,80 @@ export default {
     height: 100%;
     object-fit: 50% 50%;
   }
+}
+.button__section {
+  position: sticky;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  background-color: #f5f4f3;
+  margin-bottom: -10px;
+}
+.order-count {
+  position: relative;
+  &--sum,
+  &--minus {
+    position: relative;
+    width: 20px;
+    height: 20px;
+    margin: 0 5px;
+    border-radius: 50%;
+    background-color: rgba(217, 217, 217, 0.5);
+
+    &::before {
+      content: "";
+      position: absolute;
+      width: 70%;
+      height: 3px;
+      background-color: $primary;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+    }
+    &:active {
+      background-color: $primary;
+      box-shadow: 0 1px 5px rgba(0, 0, 0, 0.61), 0 2px 2px rgba(0, 0, 0, 0.527),
+        0 3px 1px -2px rgba(0, 0, 0, 0.514) 2px 2px 8px black;
+      &::before,
+      &::after {
+        background-color: rgba(217, 217, 217);
+      }
+    }
+  }
+  &--sum {
+    &::after {
+      content: "";
+      position: absolute;
+      width: 3px;
+      height: 70%;
+      background-color: $primary;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+    }
+  }
+}
+.add-tocart {
+  &:active {
+    background-color: rgba(156, 156, 156, 0.5);
+  }
+}
+button {
+  position: absolute;
+}
+
+.slide-up-enter-active,
+.slide-up-leave-active {
+  transition: all 0.25s ease-out;
+}
+
+.slide-up-enter-from {
+  opacity: 0;
+  transform: translateX(-30px);
+}
+
+.slide-up-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
 }
 </style>
